8b3f9fd41669ad7f16a166c36acc7e21dd5228d6
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2014-01-23 12:23:09 -0600

    Minimal files for Razor.

---
+++ MiniMonoMVC4RazorTemplate/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniMonoMVC4RazorTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ MiniMonoMVC4RazorTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniMonoMVC4RazorTemplate.Global" Language="C#" %>
